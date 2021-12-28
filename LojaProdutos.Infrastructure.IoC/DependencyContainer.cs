89c78f94d0045a21a0f3169d6f4ef2b018f30fa3
lucassilva996 <lucassilva996@outlook.com>
2021-12-28 01:52:55 -0300

    Criada a camada de injeção de dependências

---
+++ LojaProdutos.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using LojaProdutos.Application.Interfaces;
+using LojaProdutos.Application.Services;
+using LojaProdutos.Domain.Interfaces;
+using LojaProdutos.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaProdutos.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //LojaProduto.Application
+            services.AddScoped<ILojaProdutoService, LojaProdutoService>();
+            //LojaProduto.Domain.Interfaces e LojaProduto.Infra.Data.Repositories
+            services.AddScoped<ILojaProdutoRepository, LojaProdutoRepository>(); 
+        }
+    }
+}
